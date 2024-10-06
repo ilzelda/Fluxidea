011c1cc9b8a20b7b50d6831cf33d508103370817
junhee <725lsw@hanmail.net>
2024-10-06 19:44:17 +0900

    feat: Create 페이지 추가 및 저장 기능 분리
    - pageHandler 생성
    - handler들 method로 변경
    - uuid 도입

--- backend/go.mod
+++ backend/go.mod
@@ -5,4 +5,5 @@ go 1.23.0
 require (
 	github.com/go-logr/logr v1.4.2
 	github.com/go-logr/stdr v1.2.2
+	github.com/google/uuid v1.6.0
 )
--- backend/pkg/apis/pages.go
+++ backend/pkg/apis/pages.go
@@ -6,18 +6,37 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/go-logr/logr"
+	"github.com/google/uuid"
+
+	"demo/pkg/log"
 	"demo/pkg/models"
 )
 
 const dataDir string = "data"
 
-func SaveUserPage(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("user_id")
-	pageID := r.PathValue("page_id")
+var PageAPI = &pageHandler{
+	log: log.Logger.WithName("PageAPI"),
+}
 
-	var page models.Page
-	if err := json.NewDecoder(r.Body).Decode(&page); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+type pageHandler struct {
+	log logr.Logger
+}
+
+func (ph *pageHandler) RegsistRoute(mux *http.ServeMux) {
+	mux.HandleFunc("POST /api/users/{user_id}/pages", ph.createUserPage)
+	mux.HandleFunc("GET /api/users/{user_id}/pages/{page_id}", ph.loadUserPage)
+	mux.HandleFunc("PUT /api/users/{user_id}/pages/{page_id}", ph.saveUserPage)
+}
+
+func (ph *pageHandler) createUserPage(w http.ResponseWriter, r *http.Request) {
+	userID := r.PathValue("user_id")
+
+	page := &models.Page{
+		ID: uuid.New(),
+	}
+	if err := json.NewDecoder(r.Body).Decode(page); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
 		return
 	}
 	r.Body.Close()
@@ -25,21 +44,89 @@ func SaveUserPage(w http.ResponseWriter, r *http.Request) {
 	// 데이터를 저장할 디렉토리 있으면 사용 없으면 생성하여 사용
 	dirPath := filepath.Join(dataDir, userID)
 	if err := os.MkdirAll(dirPath, 0755); err != nil {
-		http.Error(w, "Failed to create data directory", http.StatusInternalServerError)
+		http.Error(w, "failed to create data directory", http.StatusInternalServerError)
 		return
 	}
 
 	// 파일 생성 및 데이터 저장
-	fileName := filepath.Join(dirPath, pageID+".json")
+	fileName := filepath.Join(dirPath, page.ID.String()+".json")
 	file, err := os.Create(fileName)
 	if err != nil {
-		http.Error(w, "Failed to create file", http.StatusInternalServerError)
+		http.Error(w, "failed to create file", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
 	if err := json.NewEncoder(file).Encode(page); err != nil {
-		http.Error(w, "Failed to save data", http.StatusInternalServerError)
+		http.Error(w, "failed to save data", http.StatusInternalServerError)
+		return
+	}
+
+	response := struct {
+		ID   uuid.UUID `json:"id"`
+		Name string    `json:"name"`
+	}{
+		ID:   page.ID,
+		Name: page.Name,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (ph *pageHandler) loadUserPage(w http.ResponseWriter, r *http.Request) {
+	userID := r.PathValue("user_id")
+	pageID := r.PathValue("page_id")
+
+	// 파일 경로 생성
+	filePath := filepath.Join(dataDir, userID, pageID+".json")
+
+	// 파일 열기
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.Error(w, "file is not exist", http.StatusNotFound)
+		} else {
+			http.Error(w, "failed to read file", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(fileContent)
+}
+
+func (ph *pageHandler) saveUserPage(w http.ResponseWriter, r *http.Request) {
+	userID := r.PathValue("user_id")
+	pageID := r.PathValue("page_id")
+
+	pageFilePath := filepath.Join(dataDir, userID, pageID+".json")
+	f, err := os.Open(pageFilePath)
+	if err != nil {
+		http.Error(w, "failed to open file", http.StatusBadRequest)
+		return
+	}
+	defer f.Close()
+
+	var page models.Page
+	if err := json.NewDecoder(f).Decode(&page); err != nil {
+		http.Error(w, "failed to decode old file data", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&page); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	f, err = os.OpenFile(pageFilePath, os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		http.Error(w, "failed to open file", http.StatusBadRequest)
+		return
+	}
+	defer f.Close()
+	if err := json.NewEncoder(f).Encode(&page); err != nil {
+		http.Error(w, "failed to encode new file data", http.StatusInternalServerError)
 		return
 	}
 
@@ -51,27 +138,4 @@ func SaveUserPage(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
-
-}
-
-func LoadUserPage(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("user_id")
-	pageID := r.PathValue("page_id")
-
-	// 파일 경로 생성
-	filePath := filepath.Join(dataDir, userID, pageID+".json")
-
-	// 파일 열기
-	fileContent, err := os.ReadFile(filePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			http.Error(w, "페이지를 찾을 수 없습니다", http.StatusNotFound)
-		} else {
-			http.Error(w, "파일을 읽을 수 없습니다", http.StatusInternalServerError)
-		}
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(fileContent)
 }
