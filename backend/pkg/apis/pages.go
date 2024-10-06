61eb9c58caf89bf42e53da963657a355afe00106
junhee <725lsw@hanmail.net>
2024-10-06 20:32:35 +0900

    feat: 특정 유저의 page 목록 조회 기능
    - log 설정

--- backend/pkg/apis/pages.go
+++ backend/pkg/apis/pages.go
@@ -25,6 +25,7 @@ type pageHandler struct {
 
 func (ph *pageHandler) RegsistRoute(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/users/{user_id}/pages", ph.createUserPage)
+	mux.HandleFunc("GET /api/users/{user_id}/pages", ph.listUserPages)
 	mux.HandleFunc("GET /api/users/{user_id}/pages/{page_id}", ph.loadUserPage)
 	mux.HandleFunc("PUT /api/users/{user_id}/pages/{page_id}", ph.saveUserPage)
 }
@@ -40,6 +41,7 @@ func (ph *pageHandler) createUserPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	r.Body.Close()
+	ph.log.Info("Create user page", "User", userID, "Page", page)
 
 	// 데이터를 저장할 디렉토리 있으면 사용 없으면 생성하여 사용
 	dirPath := filepath.Join(dataDir, userID)
@@ -73,14 +75,53 @@ func (ph *pageHandler) createUserPage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func (ph *pageHandler) listUserPages(w http.ResponseWriter, r *http.Request) {
+	userID := r.PathValue("user_id")
+
+	dirPath := filepath.Join(dataDir, userID)
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		http.Error(w, "failed to read directory", http.StatusInternalServerError)
+		return
+	}
+	ph.log.Info("List user pages", "User", userID)
+
+	type result struct {
+		ID   uuid.UUID `json:"id"`
+		Name string    `json:"name"`
+	}
+	results := make([]result, 0, len(entries))
+	// TODO: 최근 변경 시간 순으로 정렬
+	for _, entry := range entries {
+		fileName := filepath.Join(dirPath, entry.Name())
+		f, err := os.Open(fileName)
+		if err != nil {
+			http.Error(w, "failed to open file", http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		var r result
+		if err := json.NewDecoder(f).Decode(&r); err != nil {
+			http.Error(w, "failed to decode", http.StatusInternalServerError)
+			return
+		}
+		results = append(results, r)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(&results); err != nil {
+		http.Error(w, "failed to decode", http.StatusInternalServerError)
+		return
+	}
+}
+
 func (ph *pageHandler) loadUserPage(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("user_id")
 	pageID := r.PathValue("page_id")
+	ph.log.Info("Load user page", "User", userID, "PageID", pageID)
 
-	// 파일 경로 생성
 	filePath := filepath.Join(dataDir, userID, pageID+".json")
-
-	// 파일 열기
 	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -98,6 +139,7 @@ func (ph *pageHandler) loadUserPage(w http.ResponseWriter, r *http.Request) {
 func (ph *pageHandler) saveUserPage(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("user_id")
 	pageID := r.PathValue("page_id")
+	ph.log.Info("Save user page", "User", userID, "PageID", pageID)
 
 	pageFilePath := filepath.Join(dataDir, userID, pageID+".json")
 	f, err := os.Open(pageFilePath)
@@ -137,5 +179,8 @@ func (ph *pageHandler) saveUserPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
