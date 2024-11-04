0b2140be97e5b8a87d45d3918f436caa20c3c4c7
LeeBee <725lsw@hanmail.net>
2024-11-04 23:28:34 +0900

    chore: add volume for data reclaim

--- run_server.sh
+++ run_server.sh
@@ -5,9 +5,10 @@ IMAGE=mindlink.dev/mindlink:latest
 PORT=8080
 CONTAINER_PORT=8080
 CONTAINER_RUNTIME=docker
+DATA_PATH=data
 
 cp -r frontend/ backend/static/
 
 ${CONTAINER_RUNTIME} build -t ${IMAGE} backend/
 
-${CONTAINER_RUNTIME} run --name ${CONTAINER_NAME} -p ${PORT}:${CONTAINER_PORT} -d --rm ${IMAGE}
+${CONTAINER_RUNTIME} run --name ${CONTAINER_NAME} -p ${PORT}:${CONTAINER_PORT} -v ${DATA_PATH}:/app/data -d --rm ${IMAGE}
