7752aeac00e97c11fcde4fa0cc462ed3f0a4e590
highjuno <highjune@kau.kr>
2025-02-21 02:24:05 +0000

    first commit for auth(auth.js)

---
+++ frontend/auth.js
@@ -0,0 +1,22 @@
+export function handleCredentialResponse(response) {
+    console.log("Google JWT Token:", response);
+
+    // ✅ JWT를 로컬스토리지에 저장 (자동 로그인 가능)
+    // localStorage.setItem("google_jwt", response.credential);
+
+    // ✅ 서버로 JWT 전송 (사용자 검증)
+    fetch("https://0590a1e7-61ab-402e-9e7d-60cfee9e3001.mock.pstmn.io/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ token: response.credential })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("서버 응답:", data);
+    })
+    .catch(error => console.error("에러 발생:", error));
+}
+
+window.handleCredentialResponse = handleCredentialResponse;
