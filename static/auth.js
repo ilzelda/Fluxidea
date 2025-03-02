486800b0a50e53e8c42ee18209a9896b7bd84304
ilzelda-laptop <highjuno@gmail.com>
2025-03-02 12:17:51 +0000

    fix : small issue

--- static/auth.js
+++ static/auth.js
@@ -1,7 +1,5 @@
 const login_url = "/api/auth/google/login";
 
 document.getElementById("login-btn").addEventListener("click", function () {
-    document.getElementById("login-btn").addEventListener("click", function () {
-        window.location.href = login_url; // OAuth 로그인 페이지로 이동
-    });
+    window.location.href = login_url; // OAuth 로그인 페이지로 이동
 });
