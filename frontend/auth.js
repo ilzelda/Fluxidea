3590b2d4e2fa000d57a5228e7ebe9e54337bb040
highjuno <highjune@kau.kr>
2025-03-02 03:01:35 +0000

    auth.js : add postman url for test

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,21 +1,23 @@
+// const login_url = "/api/users";
+const login_url = "https://0590a1e7-61ab-402e-9e7d-60cfee9e3001.mock.pstmn.io/login";
+
+
 export function handleCredentialResponse(response) {
     console.log("Google JWT Token:", response);
 
-    // ✅ JWT를 로컬스토리지에 저장 (자동 로그인 가능)
-    // localStorage.setItem("google_jwt", response.credential);
-
     // ✅ 서버로 JWT 전송 (사용자 검증)
-    fetch("/api/users", {
+    fetch(`${login_url}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
+        credentials : "include",
         body: JSON.stringify({ token: response.credential })
     })
     .then(res => res.json())
     .then(data => {
         console.log("서버 응답:", data);
-        localStorage.setItem("mindlink_token", data);
+        localStorage.setItem("mindlink_token", JSON.stringify(data));
     })
     .catch(error => console.error("에러 발생:", error));
 }
