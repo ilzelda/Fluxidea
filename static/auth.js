eb91b1a93a67b160de85a88adab8b59f0e3559ce
ilzelda-laptop <highjuno@gmail.com>
2025-03-02 09:13:54 +0000

    feat : add eventlistener to login buttton

--- static/auth.js
+++ static/auth.js
@@ -1,25 +1,34 @@
-// const login_url = "/api/users";
-const login_url = "https://0590a1e7-61ab-402e-9e7d-60cfee9e3001.mock.pstmn.io/login";
+const login_url = "/api/auth/google/login";
+// // const login_url = "https://0590a1e7-61ab-402e-9e7d-60cfee9e3001.mock.pstmn.io/login";
 
 
-export function handleCredentialResponse(response) {
-    console.log("Google JWT Token:", response);
+// export function handleCredentialResponse(response) {
+//     console.log("Google JWT Token:", response);
 
-    // ✅ 서버로 JWT 전송 (사용자 검증)
-    fetch(`${login_url}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials : "include",
-        body: JSON.stringify({ token: response.credential })
+//     // ✅ 서버로 JWT 전송 (사용자 검증)
+//     fetch(`${login_url}`)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log("서버 응답:", data);
+//         localStorage.setItem("mindlink_token", JSON.stringify(data));
+//     })
+//     .catch(error => console.error("에러 발생:", error));
+// }
+
+// window.handleCredentialResponse = handleCredentialResponse;
+
+document.getElementById("login-btn").addEventListener("click", function () {
+    fetch(login_url)
+    .then(response => {
+        if (!response.ok) throw new Error("로그인 실패");
+        return response.json();
     })
-    .then(res => res.json())
     .then(data => {
-        console.log("서버 응답:", data);
-        localStorage.setItem("mindlink_token", JSON.stringify(data));
+        console.log("로그인 성공:", data);
+        alert("로그인 성공! 🎉");
     })
-    .catch(error => console.error("에러 발생:", error));
-}
-
-window.handleCredentialResponse = handleCredentialResponse;
+    .catch(error => {
+        console.error("에러:", error);
+        alert("로그인 실패! 😢");
+    });
+});
